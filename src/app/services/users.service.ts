51f4c7edf4823aaccb59afb981484fba1546b6b3
Roberto Marzoa <robertomarzoa@gmail.com>
2021-06-19 17:07:42 -0400

    Se crean los servicios necesarios que consumiran los datos otorgados por la api

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  url = 'http://localhost:4000/api/users/';
+
+  constructor( private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>( this.url );
+  }
+
+  deleteUser( id: string | undefined ): Observable<User> {
+    return this.http.delete<User>( this.url + id )
+  }
+
+  appUser( user: User): Observable<User> {
+    return this.http.post<User>( this.url, user );
+  }
+
+  updateUser( id: string, user: User): Observable<User> {
+    return this.http.put<User>( this.url + id, user );
+  }
+
+  getUser( id: string | undefined): Observable<User> {
+    return this.http.get<User>( this.url + id );
+  }
+
+  checkEmail( email: string ): Observable<boolean>{
+    return this.http.get<boolean>( `${this.url}check-email/${email}` );
+  }
+
+  checkDni( dni: number ): Observable<boolean>{
+    return this.http.get<boolean>( `${this.url}check-dni/${dni}` );
+  }
+
+}
